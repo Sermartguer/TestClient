21108e20887f27332dbb4562ec24cd3affb3c755
Sergio <sermartguer@gmail.com>
2021-01-13 13:55:21 +0100

    feat(views): create news and archived views

---
+++ client/src/components/Archivated/ArchivatedContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Archivated extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <div>News</div>;
+  }
+}
+
+export default Archivated;
